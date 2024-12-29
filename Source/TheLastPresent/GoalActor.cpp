241ce2d2fc2a06c469306b28c098b417811a72da
yogchiu <felixchiu99@gmail.com>
2024-12-29 10:52:16 +0000

    Added Score Sorting Polished UI

--- Source/TheLastPresent/GoalActor.cpp
+++ Source/TheLastPresent/GoalActor.cpp
@@ -37,7 +37,6 @@ void AGoalActor::Tick(float DeltaTime)
 void AGoalActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor->IsA(APresentPawn::StaticClass())) {
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("ShouldEnd"));
 		ALastPresentGameState* const GameState = GetWorld() != NULL ? GetWorld()->GetGameState<ALastPresentGameState>() : NULL;
 		if (GameState == NULL) {
 			return;
